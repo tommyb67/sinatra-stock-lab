1754a80d13c1a08ba0fa00d5b2b705053349bb35
tommyb67 <tommybrennan@gmail.com>
2013-06-25 15:18:54 -0400

    not getting it to show up

--- main.rb
+++ main.rb
@@ -3,6 +3,16 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yahoofinance'
 
-get '/' do
+
+# localhost:4567/stock_picker
+get './stock_picker' do
+  @ticker = params[:ticker]
+  @quote = params[:quote]
+  @price = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)[@ticker].lastTrade
   erb :quote
 end
+
+
+
+
+
